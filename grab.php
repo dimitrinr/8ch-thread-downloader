3a46df52899b0742e8cf2a8f3731790c00957ac7
xd <xd@xd.com>
2016-11-11 14:45:15 -0500

    error message on no url

--- grab.php
+++ grab.php
@@ -2,6 +2,12 @@
 
 $url=$argv[1];
 
+if(!isset($argv[1]))
+{
+echo "\nUsage php grab.php <thread_url>\n";
+exit;
+}
+
 $board=explode('/',$url);
 $tag=$board[3];
 
@@ -9,8 +15,9 @@ $data=file_get_contents($url);
 $base='https://media.8ch.net/'.$tag.'/src/';
 $urls=getContents($data,'<a href="https://media.8ch.net/'.$tag.'/src/','"');
 if(!count($urls))
-{$urls=getContents($data,'<a href="https://media.8ch.net/file_store/','"');
-$base="https://media.8ch.net/file_store/";
+{
+	$urls=getContents($data,'<a href="https://media.8ch.net/file_store/','"');
+	$base="https://media.8ch.net/file_store/";
 }
 $subject=getContents($data,'<span class="subject">','</span>');
 $subject=$subject[0];
